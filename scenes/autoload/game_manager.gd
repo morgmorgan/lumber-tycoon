f023933ee7d97cd9d1ff1b625490f76fef876d43
Morgan Nemeth <morgan.nemeth210@gmail.com>
2025-01-26 19:15:47 -0800

    tree juice

--- scenes/autoload/game_manager.gd
+++ scenes/autoload/game_manager.gd
@@ -3,10 +3,13 @@ extends Node
 # Player interaction
 var player_interaction_target : InteractableComponent3D
 var target_text : String
+var player_can_interact : bool
+var player_interaction_delay := 0.5 # Time in seconds
 signal interaction_target_changed(new_target : InteractableComponent3D, interaction_text : String)
 
 func _ready():
 	player_interaction_target = null
+	player_can_interact = true
 	
 func set_player_interaction_target(target : InteractableComponent3D, make_current : bool = true):
 	if !make_current:
@@ -18,9 +21,15 @@ func set_player_interaction_target(target : InteractableComponent3D, make_curren
 		target_text = player_interaction_target.interaction_text
 		
 	interaction_target_changed.emit(player_interaction_target, target_text)
-	
 
 func _input(event):
 	if event.is_action_pressed("interact"):
-		if player_interaction_target != null:
-			player_interaction_target.interacted.emit()
+		if player_can_interact:
+			if player_interaction_target != null:
+				player_interaction_target.interacted.emit()
+				interaction_delay()
+
+func interaction_delay():
+	player_can_interact = false
+	await get_tree().create_timer(player_interaction_delay).timeout
+	player_can_interact = true
--- scenes/game object/trees/basic_tree.gd
+++ scenes/game object/trees/basic_tree.gd
@@ -6,10 +6,17 @@ func _ready():
 	$InteractableComponent3D.interacted.connect(on_interacted)
 	
 func on_interacted():
+	$AnimationPlayer.play("shake")
+	$visuals/Dust.restart()
+	
 	if health >= 1:
 		health -= 1
 		MessageDeus.request_message("You gain some wood")
 		StatsDeus.add_wood(1)
 	
 	if health <= 0:
+		$InteractableComponent3D.interactable = false
+		$visuals/tree1.visible = false
+		
+		await $visuals/Dust.finished
 		queue_free()
